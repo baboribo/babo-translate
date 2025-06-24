e484f38cbac5ae54f3e77661188e1bf1148aeaf6
baboribo <inertia_mailer_0c@icloud.com>
2025-06-25 01:22:44 +0900

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let translation = '';
+
+  onMount(() => {
+    // 초기 번역 설정
+    translation = '초기 번역';
+  });
+</script>
+
+<div>
+  <div>
+    <h4>번역</h4>
+    <input type="text" bind:value={translation} placeholder="번역을 입력하세요" />
+    <button on:click={() => translation = '새로운 번역'}>번역 업데이트</button>
+  </div>
+  <div>
+    <h4>결과</h4>
+    <p>{translation}</p>
+  </div>
+</div>
